a76cc2e50dceb774090b804d1185bc4c3e7b3c88
polinakoroleva <polinakorolevaaaaa@gmail.com>
2023-05-02 10:58:10 +0700

    update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,6 +2,11 @@
 import { Command } from 'commander';
 
 const program = new Command();
-program.name('gendiff').description('Compares two configuration files and shows a difference.').version('0.8.0');
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.8.0')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format');
 
 program.parse();
