014212f06ce7893754cf1828815e9d5be5ec56ff
polinakoroleva <polinakorolevaaaaa@gmail.com>
2023-05-14 14:13:09 +0700

    add /formatters/plain.js

---
+++ src/formatters/plain.js
@@ -0,0 +1,40 @@
+import _ from 'lodash';
+
+const stringify = (value) => {
+  if (_.isObject(value)) {
+    return '[complex value]';
+  }
+  if (typeof value === 'string') {
+    return `'${value}'`;
+  }
+  return value;
+};
+
+const plain = (diff) => {
+  const iter = (data, path) => {
+    const result = data
+      .filter((node) => node.type !== 'unchanged')
+      .map((node) => {
+        const fullPath = path === '' ? `${node.key}` : `${path}.${node.key}`;
+
+        switch (node.type) {
+          case 'nested':
+            return iter(node.children, `${fullPath}`);
+          case 'added':
+            return `Property '${fullPath}' was added with value: ${stringify(node.value)}`;
+          case 'deleted':
+            return `Property '${fullPath}' was removed`;
+          case 'changed':
+            return `Property '${fullPath}' was updated. From ${stringify(node.oldValue)} to ${stringify(node.newValue)}`;
+          default:
+            throw new Error(`Unknown type: '${node.type}'!`);
+        }
+      });
+
+    return result.join('\n');
+  };
+
+  return iter(diff, '');
+};
+
+export default plain;
