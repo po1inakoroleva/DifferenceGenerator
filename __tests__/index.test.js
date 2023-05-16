fc06e3c6a4d11015b9d990887771dc202d07b593
polinakoroleva <polinakorolevaaaaa@gmail.com>
2023-05-16 14:01:07 +0700

    update tests and package.json

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -12,6 +12,12 @@ const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8'
 
 const expectedStylish = readFile('expect_stylish.txt');
 const expectedPlain = readFile('expect_plain.txt');
+const expectedJSON = readFile('expect_json.json');
+
+test('getDiff default formatter', () => {
+  expect(getDiff(getFixturePath('filepath1.json'), getFixturePath('filepath2.json'))).toEqual(expectedStylish);
+  expect(getDiff(getFixturePath('filepath1.yml'), getFixturePath('filepath2.yml'))).toEqual(expectedStylish);
+});
 
 test('getDiff stylish', () => {
   expect(getDiff(getFixturePath('filepath1.json'), getFixturePath('filepath2.json'), 'stylish')).toEqual(expectedStylish);
@@ -21,4 +27,9 @@ test('getDiff stylish', () => {
 test('getDiff plain', () => {
   expect(getDiff(getFixturePath('filepath1.json'), getFixturePath('filepath2.json'), 'plain')).toEqual(expectedPlain);
   expect(getDiff(getFixturePath('filepath1.yml'), getFixturePath('filepath2.yml'), 'plain')).toEqual(expectedPlain);
+});
+
+test('getDiff json', () => {
+  expect(getDiff(getFixturePath('filepath1.json'), getFixturePath('filepath2.json'), 'json')).toEqual(expectedJSON);
+  expect(getDiff(getFixturePath('filepath1.yml'), getFixturePath('filepath2.yml'), 'json')).toEqual(expectedJSON);
 });
