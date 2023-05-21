782f14e7d89563752871ba968ad4c8198ad0f702
polinakoroleva <polinakorolevaaaaa@gmail.com>
2023-05-21 13:41:20 +0700

    fix parsers.js

--- src/parsers.js
+++ src/parsers.js
@@ -1,18 +1,16 @@
-import fs from 'fs';
-import path from 'path';
 import { load } from 'js-yaml';
 
-const getValidPath = (filepath) => path.resolve(process.cwd(), filepath);
-const getExtname = (filepath) => path.extname(filepath);
-
-export default (filepath) => {
-  const fileExt = getExtname(filepath);
-
-  if (fileExt === '.json') {
-    return JSON.parse(fs.readFileSync(getValidPath(filepath), 'utf8'));
+const getParsedContent = (data, ext) => {
+  switch (ext) {
+    case '.json':
+      return JSON.parse(data);
+    case '.yaml':
+      return load(data);
+    case '.yml':
+      return load(data);
+    default:
+      throw new Error(`File extension ${ext} is incorrect!`);
   }
-  if (fileExt === '.yaml' || fileExt === '.yml') {
-    return load(fs.readFileSync(getValidPath(filepath), 'utf8'));
-  }
-  throw new Error(`Unknown format: '${fileExt}'!`);
 };
+
+export default getParsedContent;
