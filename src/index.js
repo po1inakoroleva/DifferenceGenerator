d6b97054a2535aa426b92b80bc817a2c04d833d8
polinakoroleva <polinakorolevaaaaa@gmail.com>
2023-05-21 13:40:37 +0700

    moved and fix makeAstTree

--- src/index.js
+++ src/index.js
@@ -1,43 +1,18 @@
-import _ from 'lodash';
-import parser from './parsers.js';
+import fs from 'fs';
+import path from 'path';
+import makeAstTree from './makeAstTree.js';
+import getParsedContent from './parsers.js';
 import getFormat from './formatters/index.js';
 
-const genDiff = (data1, data2) => {
-  const keys = _.sortBy(_.union(_.keys(data1), _.keys(data2)));
+const getValidPath = (filepath) => path.resolve(process.cwd(), filepath);
+const getFileData = (filepath) => fs.readFileSync(getValidPath(filepath), 'utf8');
+const getExtname = (filepath) => path.extname(filepath);
 
-  const result = keys.map((key) => {
-    const value1 = data1[key];
-    const value2 = data2[key];
-
-    if (_.isObject(value1) && _.isObject(value2)) {
-      return { key, children: genDiff(value1, value2), type: 'nested' };
-    }
-    if (!_.has(data1, key)) {
-      return { key, value: value2, type: 'added' };
-    }
-    if (!_.has(data2, key)) {
-      return { key, value: value1, type: 'deleted' };
-    }
-    if (value1 !== value2) {
-      return {
-        key,
-        oldValue: data1[key],
-        newValue: data2[key],
-        type: 'changed',
-      };
-    }
-
-    return { key, value: data1[key], type: 'unchanged' };
-  });
-
-  return result;
-};
-
-const getDiff = (filepath1, filepath2, formatName = 'stylish') => {
-  const data1 = parser(filepath1);
-  const data2 = parser(filepath2);
+const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
+  const data1 = getParsedContent(getFileData(filepath1), getExtname(filepath1));
+  const data2 = getParsedContent(getFileData(filepath2), getExtname(filepath2));
   const format = getFormat(formatName);
-  return format(genDiff(data1, data2));
+  return format(makeAstTree(data1, data2));
 };
 
-export default getDiff;
+export default genDiff;
